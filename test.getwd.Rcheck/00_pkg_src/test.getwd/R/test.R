ec36af1562f26e41cb6d05e6cfe051e786be69d0
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-11-07 23:50:17 +0100

    print system.file and getNamespaceName results

--- test.getwd.Rcheck/00_pkg_src/test.getwd/R/test.R
+++ test.getwd.Rcheck/00_pkg_src/test.getwd/R/test.R
@@ -1,1 +1,4 @@
 message("In package: ", getwd())
+message("system.file: ", system.file(".", package = "test.getwd"))
+m <- function() message("getNamespaceName: ", getNamespaceName(parent.frame()))
+m()
